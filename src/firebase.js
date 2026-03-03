d3f008712d95d1e8b3b217ec87c936446411f593
NolanKnight <nolan.r.knight@gmail.com>
2026-03-03 12:08:14 -0800

    using different api keys for local and deployment

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,10 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY === undefined ? process.env.NEXT_PUBLIC_FIREBASE_API_KEY : process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "haikuatry.firebaseapp.com",
     databaseURL: "https://haikuatry-default-rtdb.firebaseio.com",
     projectId: "haikuatry",
