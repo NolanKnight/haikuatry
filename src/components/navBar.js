95b1a6b2dec45c3becdfc43a7c989d6bb4e2668f
Nolan Knight <91164617+NolanKnight@users.noreply.github.com>
2026-03-09 10:17:49 -0700

    Mobile Nav (#9)
    * updated nav bar ui
    
    * color transition on nav bar hover
    
    * fixed post button
    
    * movile nav WIP
    
    * working mobile nav

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -32,46 +32,47 @@ const NavBar = ({ layoutRef }) => {
     element.addEventListener("scroll", handleScroll);
 
     return () => element.removeEventListener("scroll", handleScroll);
-  }, []);
+  }, [layoutRef]);
 
   return (
     <div className="fixed h-36 w-full grid grid-cols-10 place-items-center font-montserrat">
       <NavLink
         to="/"
         className={({ isActive }) =>
-          `text-3xl text-blue-800 col-span-3 border-b-4 ${isActive ? "border-blue-800" : "border-transparent"}`
+          `text-3xl button col-span-3 border-b-4 ${isActive ? "border-blue-800" : "border-transparent"}`
         }
       >
         <b>HAIKUATRY</b>
       </NavLink>
-      <div className="col-span-3"></div>
-      {[
-        { title: "FEED", path: "/feed", isCompact: "translate-x-[20vw]" },
-        {
-          title: "POST",
-          path: "/post",
-          isCompact: "translate-x-[10vw] translate-y-10",
-        },
-      ].map((link) => (
-        <NavLink
-          to={link.path}
-          className={({ isActive }) =>
-            `text-xl w-24 text-center text-blue-800 border-b-4 ${isActive ? "border-blue-800" : "border-transparent"} transition-transform duration-500 ease-in-out ${
-              isCompact() ? link.isCompact : "translate-x-0 translate-y-0"
-            }`
-          }
+      <div className="col-start-6 col-span-4 w-full h-full grid place-items-center grid-cols-3">
+        {[
+          { title: "FEED", path: "/feed", isCompact: "translate-x-[26.67vw]" },
+          {
+            title: "POST",
+            path: "/post",
+            isCompact: "translate-x-[13.33vw] translate-y-10",
+          },
+        ].map((link) => (
+          <NavLink
+            to={link.path}
+            className={({ isActive }) =>
+              `text-lg px-4 text-center button border-b-4 ${isActive ? "border-blue-800" : "border-transparent"} ${
+                isCompact() ? link.isCompact : "translate-x-0 translate-y-0"
+              }`
+            }
+          >
+            <b>{link.title}</b>
+          </NavLink>
+        ))}
+        <button
+          onClick={user ? signOut : googleSignIn}
+          className={`text-lg px-4 text-center button border-b-4 border-transparent ${
+            isCompact() ? "translate-y-20" : "translate-y-0"
+          }`}
         >
-          <b>{link.title}</b>
-        </NavLink>
-      ))}
-      <button
-        onClick={user ? signOut : googleSignIn}
-        className={`text-xl w-24 text-center text-blue-800 border-b-4 border-transparent transition-transform duration-500 ease-in-out ${
-          isCompact() ? "translate-y-20" : "translate-y-0"
-        }`}
-      >
-        <b>{user ? "SIGN OUT" : "SIGN IN"}</b>
-      </button>
+          <b>{user ? "SIGN OUT" : "SIGN IN"}</b>
+        </button>
+      </div>
     </div>
   );
 };
