95bec284c48952edf8017526e45021b16294c168
Nolan Knight <91164617+NolanKnight@users.noreply.github.com>
2026-05-14 11:00:51 -0700

    Fix Display (#11)
    * fixed display but resize does not work
    
    * resizing works for nav bar display
    
    * page width
    
    * fixed initial resize issue
    
    * prettier

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,8 +12,8 @@ const Layout = ({ children }) => {
         className="w-full h-screen overflow-y-auto scrollbar-gutter bg-yellow-100 sm:block hidden"
       >
         <NavBar layoutRef={ref} />
-        <div className="w-full grid grid-cols-3 place-items-center">
-          <div id="page" className="w-full mt-36 col-start-2">
+        <div className="w-full grid grid-cols-10 place-items-center">
+          <div id="page" className="w-full col-start-4 col-span-4">
             {children}
           </div>
         </div>
